de6e1b41fb48dcbe031ead82f849cc5e19853b51
DongjaeLee-Dd2dD2 <143481970+DongjaeLee-Dd2dD2@users.noreply.github.com>
2023-09-01 13:10:42 +0900

    Update exhaustive_calculation_of_fpp.py

--- Toy_Experiments/exhaustive_calculation_of_fpp.py
+++ Toy_Experiments/exhaustive_calculation_of_fpp.py
@@ -1,43 +1,59 @@
 import math
 
 fact = []
-fact.append(1)
-for i in range(1,256+1):
-    fact.append(fact[i-1]*i)
 
-fo = open("Exhaustive_Calculation_Result_AES.txt", "w")
-#fo = open("Exhaustive_Calculation_Result_AES.txt", "w")
+def build_fact(n = 256):
+    fact.append(1)
+    for i in range(1, n+1):
+        fact.append(fact[-1]*i)
 
-ulist = [256, 128, 64, 32, 16, 8, 4, 2]
-vlist = [256, 128, 64, 32, 16, 8, 4, 2]
+build_fact()
 
-for u in ulist:
-    for v in vlist:
-        print('u: ', u, ', v: ', v)
-        fo.write('u: ' + str(u) + ', v: ' + str(v) + '\n')
-        sm = 0
-        idx = 0
-        f = open("sampling_result_AES", "r")
-        lines = f.readlines()
-        for line in lines:
-            idx += 1
-            if idx%65536 == 0:
-                print('idx: ', idx)
-            tmp = fact[u]
-            a = [0]*v
-            
-            for j in range(0,u):
-                a[int(line.split()[j])%v] += 1
-            
-            for j in range(0,v):
-                tmp //= fact[a[j]]
-            sm += tmp
-        f.close()
-        print(sm)
-        fo.write(str(sm) + '\n')
-        print(math.log2(sm))
-        fo.write(str(math.log2(sm)) + '\n')
-        print(math.log2(sm)-math.log2(v)*u)
-        fo.write(str(math.log2(sm)-math.log2(v)*u) + '\n')
+uv = [(256,8), (256,7), (256,6), (256,5), (256,4), (256,3), (128,8), (128,7), (128,6), (128,5), (128,4), (128,3), (64,8), (64,7), (64,6), (64,5), (64,4), (64,3), (32,8), (32,7), (32,6), (32,5), (32,4), (16,8), (16,7), (16,6), (16,5), (16,4), (8,8), (8,7), (8,6), (8,5), (4,8), (4,7)]
+
+for (u,v) in uv:
+    print('u: ', u, ', v: ', v)
+    
+    vv = int(2**v)
+
+    sm = 0
+    idx = 0
+    f = open("Sampling_Result_AES.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        idx += 1
+        if idx%65536 == 0:
+            print('idx: ', idx)
+        tmp = fact[u]
+        a = [0]*vv
+        
+        for j in range(0,u):
+            a[int(line.split()[j])%vv] += 1
+        
+        for j in range(0,vv):
+            tmp //= fact[a[j]]
+        sm += tmp
+    f.close()
+    print("fpp_AES: ", math.log2(sm)-v*u)
+    
+    sm = 0
+    idx = 0
+    f = open("Sampling_Result_ARIA.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        idx += 1
+        if idx%65536 == 0:
+            print('idx: ', idx)
+        tmp = fact[u]
+        a = [0]*vv
+        
+        for j in range(0,u):
+            a[int(line.split()[j])%vv] += 1
+        
+        for j in range(0,vv):
+            tmp //= fact[a[j]]
+        sm += tmp
+    f.close()
+    print("fpp_ARIA: ", math.log2(sm)-v*u)
 
 fo.close()
