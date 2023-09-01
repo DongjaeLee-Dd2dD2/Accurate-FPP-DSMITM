9a1f31adf845dd48495a5fc531761f6e9e80b711
DongjaeLee-Dd2dD2 <143481970+DongjaeLee-Dd2dD2@users.noreply.github.com>
2023-09-01 11:20:21 +0900

    Update Sampling_Sequences_AES.cpp

--- Toy_Experiments/Sampling_Sequences_AES.cpp
+++ Toy_Experiments/Sampling_Sequences_AES.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int X[6][16], Y[6][16], Z[6][16], W[6][16], XX[6][16], YY[6][16], ZZ[6][16], WW[6][16], DX[6][16], DY[6][16], DZ[6][16], DW[6][16];
+// Variables for First Phase
+// In Codes <=> In Paper
+// xx, yy, zz, ww <=> x', y', z', w'
+// x[i][j] <=> x_i[j]
+int x[6][16], y[6][16], z[6][16], w[6][16], xx[6][16], yy[6][16], zz[6][16], ww[6][16], dx[6][16], dy[6][16], dz[6][16], dw[6][16];
+
+// Variables for Second Phase
+// In Codes <=> In Paper
+// X[i][j] <=> X_1^i[j], X_2^i[j], X_3^i[j], X_4^i[j] (We reuse the variable X[i][j])
+// DX <=> Delta X
+int X[256][16], Y[256][16], Z[256][16], W[256][16], DX[256][16], DY[256][16], DZ[256][16], DW[256][16];
+
+// S-box, Inverse S-box
 int Sbox[256] = {
 	0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
 	0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
@@ -38,19 +50,47 @@ int iSbox[256] = {
 	0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
 	0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
 };
-int x[256][16], y[256][16], z[256][16], w[256][16], dx[256][16], dy[256][16], dz[256][16], dw[256][16];
-vector<int> DDT[256][256];
-FILE* in;
-int seq_cnt = 0;
 
-void build_DDT() {
-	for (int i = 0; i < 256; i++) {
-		for (int j = i + 1; j < 256; j++) {
-			DDT[i ^ j][Sbox[i] ^ Sbox[j]].push_back(i);
-			DDT[i ^ j][Sbox[i] ^ Sbox[j]].push_back(j);
+// Differential Distribution Table
+vector<int> DDT[256][256];
+
+// For File Output
+FILE* in;
+
+// For counting gathered sequences
+int seq_cnt = 0;
+int sampling_threshold = 1 << 20;
+
+// Multiplication on GF(2^8)
+int MUL(int a, int b);
+
+// Construct DDT
+void build_DDT();
+
+// Step 1. of First Phase.
+void get_10_params();
+
+// Step 2. of First Phase + Second Phase
+void get_24_params();
+
+// Second Phase.
+void get_sequence();
+
+// main
+int main() {
+	in = fopen("sampling_result_AES.txt", "w");
+	build_DDT();
+	while (true) {
+		get_10_params(); // Step 1. of First Phase
+		get_24_params(); // Step 2. of First Phase + Second Phase
+		if (seq_cnt == sampling_threshold) {
+			fclose(in);
+			break;
 		}
 	}
 }
+
+// Multiplication on GF(2^8)
 int MUL(int a, int b) {
 	int ret = 0;
 	for (int i = 0; i < 8; i++) {
@@ -62,220 +102,174 @@ int MUL(int a, int b) {
 
 	return ret;
 }
-void get_10_params() {
-	DZ[1][0] = rand() % 255 + 1;
-	X[2][0] = rand() % 256;
-	X[2][1] = rand() % 256;
-	X[2][2] = rand() % 256;
-	X[2][3] = rand() % 256;
-	DW[4][0] = rand() % 255 + 1;
-	Z[4][0] = rand() % 256;
-	Z[4][1] = rand() % 256;
-	Z[4][2] = rand() % 256;
-	Z[4][3] = rand() % 256;
-}
-void get_sequence() {
-	vector<int> cnt(256, 0);
+
+// Construct DDT
+void build_DDT() {
 	for (int i = 0; i < 256; i++) {
-		dz[i][0] = i;
-
-		dw[i][0] = MUL(dz[i][0], 0x02);
-		dw[i][1] = MUL(dz[i][0], 0x03);
-		dw[i][2] = dz[i][0];
-		dw[i][3] = dz[i][0];
-
-		x[i][0] = X[2][0] ^ dw[i][0];
-		x[i][1] = X[2][1] ^ dw[i][1];
-		x[i][2] = X[2][2] ^ dw[i][2];
-		x[i][3] = X[2][3] ^ dw[i][3];
-
-		z[i][0] = Sbox[x[i][0]];
-		z[i][13] = Sbox[x[i][1]];
-		z[i][10] = Sbox[x[i][2]];
-		z[i][7] = Sbox[x[i][3]];
-
-		dz[i][0] = z[0][0] ^ z[i][0];
-		dz[i][13] = z[0][13] ^ z[i][13];
-		dz[i][10] = z[0][10] ^ z[i][10];
-		dz[i][7] = z[0][7] ^ z[i][7];
-
-		dw[i][0] = MUL(dz[i][0], 0x02);
-		dw[i][1] = dz[i][0];
-		dw[i][2] = dz[i][0];
-		dw[i][3] = MUL(dz[i][0], 0x03);
-
-		dw[i][4] = dz[i][7];
-		dw[i][5] = dz[i][7];
-		dw[i][6] = MUL(dz[i][7], 0x03);
-		dw[i][7] = MUL(dz[i][7], 0x02);
-
-		dw[i][8] = dz[i][10];
-		dw[i][9] = MUL(dz[i][10], 0x03);
-		dw[i][10] = MUL(dz[i][10], 0x02);
-		dw[i][11] = dz[i][10];
-
-		dw[i][12] = MUL(dz[i][13], 0x03);
-		dw[i][13] = MUL(dz[i][13], 0x02);
-		dw[i][14] = dz[i][13];
-		dw[i][15] = dz[i][13];
-
-		for (int j = 0; j < 16; j++) x[i][j] = X[3][j] ^ dw[i][j];
-
-		for (int j = 0; j < 16; j++) z[i][j] = Sbox[x[i][(j + (j % 4) * 4) % 16]];
-
-		for (int j = 0; j < 16; j++) dz[i][j] = z[0][j] ^ z[i][j];
-
-		dw[i][0] = MUL(dz[i][0], 0x02) ^ MUL(dz[i][1], 0x03) ^ dz[i][2] ^ dz[i][3];
-		dw[i][5] = dz[i][4] ^ MUL(dz[i][5], 0x02) ^ MUL(dz[i][6], 0x03) ^ dz[i][7];
-		dw[i][10] = dz[i][8] ^ dz[i][9] ^ MUL(dz[i][10], 0x02) ^ MUL(dz[i][11], 0x03);
-		dw[i][15] = MUL(dz[i][12], 0x03) ^ dz[i][13] ^ dz[i][14] ^ MUL(dz[i][15], 0x02);
-
-		x[i][0] = X[4][0] ^ dw[i][0];
-		x[i][5] = X[4][5] ^ dw[i][5];
-		x[i][10] = X[4][10] ^ dw[i][10];
-		x[i][15] = X[4][15] ^ dw[i][15];
-
-		z[i][0] = Sbox[x[i][0]];
-		z[i][1] = Sbox[x[i][5]];
-		z[i][2] = Sbox[x[i][10]];
-		z[i][3] = Sbox[x[i][15]];
-
-		dz[i][0] = z[0][0] ^ z[i][0];
-		dz[i][1] = z[0][1] ^ z[i][1];
-		dz[i][2] = z[0][2] ^ z[i][2];
-		dz[i][3] = z[0][3] ^ z[i][3];
-
-		dw[i][0] = MUL(dz[i][0], 0x02) ^ MUL(dz[i][1], 0x03) ^ dz[i][2] ^ dz[i][3];
-		fprintf(in, "%d ", dw[i][0]);
-		//cnt[dw[i][0]]++;
+		for (int j = i + 1; j < 256; j++) {
+			DDT[i ^ j][Sbox[i] ^ Sbox[j]].push_back(i);
+			DDT[i ^ j][Sbox[i] ^ Sbox[j]].push_back(j);
+		}
 	}
-	//for (int i = 0; i < 256; i++) fprintf(in, "%d ", cnt[i]);
-	fprintf(in, "\n");
-	seq_cnt++;
 }
+
+// Step 1. of First Phase.
+void get_10_params() {
+	dz[1][0] = rand() % 255 + 1;
+	x[2][0] = rand() % 256;
+	x[2][1] = rand() % 256;
+	x[2][2] = rand() % 256;
+	x[2][3] = rand() % 256;
+	dw[4][0] = rand() % 255 + 1;
+	z[4][0] = rand() % 256;
+	z[4][1] = rand() % 256;
+	z[4][2] = rand() % 256;
+	z[4][3] = rand() % 256;
+}
+
+// Step 2. of First Phase + Second Phase
 void get_24_params() {
-	XX[2][0] = X[2][0] ^ MUL(DZ[1][0], 0x02);
-	XX[2][1] = X[2][1] ^ DZ[1][0];
-	XX[2][2] = X[2][2] ^ DZ[1][0];
-	XX[2][3] = X[2][3] ^ MUL(DZ[1][0], 0x03);
+	// Step 2-(a). of First Phase.
+	// dz[1][0] -> dx[2][0,1,2,3] 
+	dx[2][0] = MUL(dz[1][0], 0x02);
+	dx[2][1] = dz[1][0];
+	dx[2][2] = dz[1][0];
+	dx[2][3] = MUL(dz[1][0], 0x03);
 
-	for (int i = 0; i < 4; i++) Y[2][i] = Sbox[X[2][i]];
-	for (int i = 0; i < 4; i++) YY[2][i] = Sbox[XX[2][i]];
-	for (int i = 0; i < 4; i++) DY[2][i] = Y[2][i] ^ YY[2][i];
+	// Step 2-(b). of First Phase.
+	// x[2][0,1,2,3], dx[2][0,1,2,3] -> x'[2][0,1,2,3]
+	xx[2][0] = x[2][0] ^ dx[2][0];
+	xx[2][1] = x[2][1] ^ dx[2][1];
+	xx[2][2] = x[2][2] ^ dx[2][2];
+	xx[2][3] = x[2][3] ^ dx[2][3];
 
-	DZ[2][0] = DY[2][0];
-	DZ[2][13] = DY[2][1];
-	DZ[2][10] = DY[2][2];
-	DZ[2][7] = DY[2][3];
+	// x[2][0,1,2,3], x'[0,1,2,3] -> dz[2][0,7,10,13]
+	for (int i = 0; i < 4; i++) y[2][i] = Sbox[x[2][i]];
+	for (int i = 0; i < 4; i++) yy[2][i] = Sbox[xx[2][i]];
+	for (int i = 0; i < 4; i++) dy[2][i] = y[2][i] ^ yy[2][i];
 
-	DX[3][0] = MUL(DZ[2][0], 0x02);
-	DX[3][1] = DZ[2][0];
-	DX[3][2] = DZ[2][0];
-	DX[3][3] = MUL(DZ[2][0], 0x03);
-	
-	DX[3][4] = DZ[2][7];
-	DX[3][5] = DZ[2][7];
-	DX[3][6] = MUL(DZ[2][7], 0x03);
-	DX[3][7] = MUL(DZ[2][7], 0x02);
+	dz[2][0] = dy[2][0];
+	dz[2][13] = dy[2][1];
+	dz[2][10] = dy[2][2];
+	dz[2][7] = dy[2][3];
 
-	DX[3][8] = DZ[2][10];
-	DX[3][9] = MUL(DZ[2][10], 0x03);
-	DX[3][10] = MUL(DZ[2][10], 0x02);
-	DX[3][11] = DZ[2][0];
+	// dz[2][0,7,10,13] -> dx[3]
+	dx[3][0] = MUL(dz[2][0], 0x02);
+	dx[3][1] = dz[2][0];
+	dx[3][2] = dz[2][0];
+	dx[3][3] = MUL(dz[2][0], 0x03);
 
-	DX[3][12] = MUL(DZ[2][13], 0x03);
-	DX[3][13] = MUL(DZ[2][13], 0x02);
-	DX[3][14] = DZ[2][13];
-	DX[3][15] = DZ[2][13];
+	dx[3][4] = dz[2][7];
+	dx[3][5] = dz[2][7];
+	dx[3][6] = MUL(dz[2][7], 0x03);
+	dx[3][7] = MUL(dz[2][7], 0x02);
 
-	Y[4][0] = Z[4][0];
-	Y[4][5] = Z[4][1];
-	Y[4][10] = Z[4][2];
-	Y[4][15] = Z[4][3];
+	dx[3][8] = dz[2][10];
+	dx[3][9] = MUL(dz[2][10], 0x03);
+	dx[3][10] = MUL(dz[2][10], 0x02);
+	dx[3][11] = dz[2][0];
 
-	for (int i = 0; i < 4; i++) X[4][i * 5] = iSbox[Y[4][i * 5]];
-	
-	DZ[4][0] = MUL(DW[4][0], 0x0e);
-	DZ[4][1] = MUL(DW[4][0], 0x09);
-	DZ[4][2] = MUL(DW[4][0], 0x0d);
-	DZ[4][3] = MUL(DW[4][0], 0x0b);
+	dx[3][12] = MUL(dz[2][13], 0x03);
+	dx[3][13] = MUL(dz[2][13], 0x02);
+	dx[3][14] = dz[2][13];
+	dx[3][15] = dz[2][13];
 
-	for (int i = 0; i < 4; i++) ZZ[4][i] = Z[4][i] ^ DZ[4][i];
+	// Steo 2-(c). of First Phase.
+	// dw[4][0] -> dz[4][0,1,2,3]
+	dz[4][0] = MUL(dw[4][0], 0x0e);
+	dz[4][1] = MUL(dw[4][0], 0x09);
+	dz[4][2] = MUL(dw[4][0], 0x0d);
+	dz[4][3] = MUL(dw[4][0], 0x0b);
 
-	YY[4][0] = ZZ[4][0];
-	YY[4][5] = ZZ[4][1];
-	YY[4][10] = ZZ[4][2];
-	YY[4][15] = ZZ[4][3];
+	// z[4][0,1,2,3], dz[4][0,1,2,3] -> x[4][0,5,10,15], x'[4][0,5,10,15]
+	for (int i = 0; i < 4; i++) zz[4][i] = z[4][i] ^ dz[4][i];
 
-	for (int i = 0; i < 4; i++) XX[4][i * 5] = iSbox[YY[4][i * 5]];
+	y[4][0] = z[4][0];
+	y[4][5] = z[4][1];
+	y[4][10] = z[4][2];
+	y[4][15] = z[4][3];
+	yy[4][0] = zz[4][0];
+	yy[4][5] = zz[4][1];
+	yy[4][10] = zz[4][2];
+	yy[4][15] = zz[4][3];
 
-	for (int i = 0; i < 4; i++) DX[4][i * 5] = X[4][i * 5] ^ XX[4][i * 5];
+	for (int i = 0; i < 4; i++) x[4][i * 5] = iSbox[y[4][i * 5]];
+	for (int i = 0; i < 4; i++) xx[4][i * 5] = iSbox[yy[4][i * 5]];
 
-	for (int i = 0; i < 4; i++) DW[3][i * 5] = DX[4][i * 5];
+	// Step 2-(d). of First Phase.
+	// x[4][0,5,10,15], x'[4][0,5,10,15] -> dx[4][0,5,10,15]
+	for (int i = 0; i < 4; i++) dx[4][i * 5] = x[4][i * 5] ^ xx[4][i * 5];
 
-	DZ[3][0] = MUL(DW[3][0], 0x0e);
-	DZ[3][1] = MUL(DW[3][0], 0x09);
-	DZ[3][2] = MUL(DW[3][0], 0x0d);
-	DZ[3][3] = MUL(DW[3][0], 0x0b);
+	// dx[4][0,5,10,15] -> dy[3]
+	for (int i = 0; i < 4; i++) dw[3][i * 5] = dx[4][i * 5];
 
-	DZ[3][4] = MUL(DW[3][5], 0x0b);
-	DZ[3][5] = MUL(DW[3][5], 0x0e);
-	DZ[3][6] = MUL(DW[3][5], 0x09);
-	DZ[3][7] = MUL(DW[3][5], 0x0d);
+	dz[3][0] = MUL(dw[3][0], 0x0e);
+	dz[3][1] = MUL(dw[3][0], 0x09);
+	dz[3][2] = MUL(dw[3][0], 0x0d);
+	dz[3][3] = MUL(dw[3][0], 0x0b);
 
-	DZ[3][8] = MUL(DW[3][10], 0x0d);
-	DZ[3][9] = MUL(DW[3][10], 0x0b);
-	DZ[3][10] = MUL(DW[3][10], 0x0d);
-	DZ[3][11] = MUL(DW[3][10], 0x09);
+	dz[3][4] = MUL(dw[3][5], 0x0b);
+	dz[3][5] = MUL(dw[3][5], 0x0e);
+	dz[3][6] = MUL(dw[3][5], 0x09);
+	dz[3][7] = MUL(dw[3][5], 0x0d);
 
-	DZ[3][12] = MUL(DW[3][15], 0x09);
-	DZ[3][13] = MUL(DW[3][15], 0x0d);
-	DZ[3][14] = MUL(DW[3][15], 0x0b);
-	DZ[3][15] = MUL(DW[3][15], 0x0e);
+	dz[3][8] = MUL(dw[3][10], 0x0d);
+	dz[3][9] = MUL(dw[3][10], 0x0b);
+	dz[3][10] = MUL(dw[3][10], 0x0d);
+	dz[3][11] = MUL(dw[3][10], 0x09);
 
-	for (int i = 0; i < 16; i++) DY[3][i] = DZ[3][(16 + i - (i % 4) * 5) % 16];
+	dz[3][12] = MUL(dw[3][15], 0x09);
+	dz[3][13] = MUL(dw[3][15], 0x0d);
+	dz[3][14] = MUL(dw[3][15], 0x0b);
+	dz[3][15] = MUL(dw[3][15], 0x0e);
 
-	/*for (int i = 0; i < 16; i++) printf("DX[3][%d]: %d, DY[3][%d]: %d\n", i, DX[3][i], i, DY[3][i]);
-	getchar();*/
+	for (int i = 0; i < 16; i++) dy[3][i] = dz[3][(16 + i - (i % 4) * 5) % 16];
 
+	// Step 2-(e). of First Phase.
+	// Solve 16 S-box differential equations between DX[3] and DY[3]
+
+	// No solution is found
 	for (int i = 0; i < 16; i++) {
-		if (DDT[DX[3][i]][DY[3][i]].size() == 0) return;
+		if (DDT[dx[3][i]][dy[3][i]].size() == 0) return;
 	}
-	printf("found!\n");
-	for (auto i0 : DDT[DX[3][0]][DY[3][0]]) {
-		for (auto i1 : DDT[DX[3][1]][DY[3][1]]) {
-			for (auto i2 : DDT[DX[3][2]][DY[3][2]]) {
-				for (auto i3 : DDT[DX[3][3]][DY[3][3]]) {
-					for (auto i4 : DDT[DX[3][4]][DY[3][4]]) {
-						for (auto i5 : DDT[DX[3][5]][DY[3][5]]) {
-							for (auto i6 : DDT[DX[3][6]][DY[3][6]]) {
-								for (auto i7 : DDT[DX[3][7]][DY[3][7]]) {
-									for (auto i8 : DDT[DX[3][8]][DY[3][8]]) {
-										for (auto i9 : DDT[DX[3][9]][DY[3][9]]) {
-											for (auto i10 : DDT[DX[3][10]][DY[3][10]]) {
-												for (auto i11 : DDT[DX[3][11]][DY[3][11]]) {
-													for (auto i12 : DDT[DX[3][12]][DY[3][12]]) {
-														for (auto i13 : DDT[DX[3][13]][DY[3][13]]) {
-															for (auto i14 : DDT[DX[3][14]][DY[3][14]]) {
-																for (auto i15 : DDT[DX[3][15]][DY[3][15]]) {
-																	X[3][0] = i0;
-																	X[3][1] = i1;
-																	X[3][2] = i2;
-																	X[3][3] = i3;
-																	X[3][4] = i4;
-																	X[3][5] = i5;
-																	X[3][6] = i6;
-																	X[3][7] = i7;
-																	X[3][8] = i8;
-																	X[3][9] = i9;
-																	X[3][10] = i10;
-																	X[3][11] = i11;
-																	X[3][12] = i12;
-																	X[3][13] = i13;
-																	X[3][14] = i14;
-																	X[3][15] = i15;
-																	get_sequence();
-																	if (seq_cnt == 1048576) return;
+
+	// Solutions are found
+	for (auto i0 : DDT[dx[3][0]][dy[3][0]]) {
+		for (auto i1 : DDT[dx[3][1]][dy[3][1]]) {
+			for (auto i2 : DDT[dx[3][2]][dy[3][2]]) {
+				for (auto i3 : DDT[dx[3][3]][dy[3][3]]) {
+					for (auto i4 : DDT[dx[3][4]][dy[3][4]]) {
+						for (auto i5 : DDT[dx[3][5]][dy[3][5]]) {
+							for (auto i6 : DDT[dx[3][6]][dy[3][6]]) {
+								for (auto i7 : DDT[dx[3][7]][dy[3][7]]) {
+									for (auto i8 : DDT[dx[3][8]][dy[3][8]]) {
+										for (auto i9 : DDT[dx[3][9]][dy[3][9]]) {
+											for (auto i10 : DDT[dx[3][10]][dy[3][10]]) {
+												for (auto i11 : DDT[dx[3][11]][dy[3][11]]) {
+													for (auto i12 : DDT[dx[3][12]][dy[3][12]]) {
+														for (auto i13 : DDT[dx[3][13]][dy[3][13]]) {
+															for (auto i14 : DDT[dx[3][14]][dy[3][14]]) {
+																for (auto i15 : DDT[dx[3][15]][dy[3][15]]) {
+																	x[3][0] = i0;
+																	x[3][1] = i1;
+																	x[3][2] = i2;
+																	x[3][3] = i3;
+																	x[3][4] = i4;
+																	x[3][5] = i5;
+																	x[3][6] = i6;
+																	x[3][7] = i7;
+																	x[3][8] = i8;
+																	x[3][9] = i9;
+																	x[3][10] = i10;
+																	x[3][11] = i11;
+																	x[3][12] = i12;
+																	x[3][13] = i13;
+																	x[3][14] = i14;
+																	x[3][15] = i15;
+
+																	// Prceed to the Second Phase with each solution
+																	get_sequence(); // <- Second Phase
+																	if (seq_cnt == sampling_threshold) return;
 																}
 															}
 														}
@@ -294,16 +288,103 @@ void get_24_params() {
 	}
 }
 
+// Second Phase.
+void get_sequence() {
+	for (int i = 0; i < 256; i++) {
+		// Step 1-(a). of Second Phase.
+		// Assign i to DZ_1^i[0]
+		DZ[i][0] = i;
 
-int main() {
-	in = fopen("sampling_result_AES.txt", "w");
-	build_DDT();
-	while (true) {
-		get_10_params();
-		get_24_params();
-		if (seq_cnt == 1048576) {
-			fclose(in);
-			break;
-		}
+		// DZ_1^i[0] -> DX_2^i[0,1,2,3]
+		DX[i][0] = MUL(DZ[i][0], 0x02);
+		DX[i][1] = MUL(DZ[i][0], 0x03);
+		DX[i][2] = DZ[i][0];
+		DX[i][3] = DZ[i][0];
+
+		// Step 1-(b). of Second Phase.
+		// X_2^0[0,1,2,3], DX_2^i[0,1,2,3] -> X_2^i[0,1,2,3]
+		X[i][0] = x[2][0] ^ DX[i][0];
+		X[i][1] = x[2][1] ^ DX[i][1];
+		X[i][2] = x[2][2] ^ DX[i][2];
+		X[i][3] = x[2][3] ^ DX[i][3];
+
+		// X_2^i[0,1,2,3] -> Z_2^i[0,7,10,13]
+		Z[i][0] = Sbox[X[i][0]];
+		Z[i][13] = Sbox[X[i][1]];
+		Z[i][10] = Sbox[X[i][2]];
+		Z[i][7] = Sbox[X[i][3]];
+
+		// Step 1-(c). of Second Phase.
+		// Z_2^0[0,7,10,13], Z_2^i[0,7,10,13] -> DZ_2^i[0,7,10,13]
+		DZ[i][0] = Z[0][0] ^ Z[i][0];
+		DZ[i][13] = Z[0][13] ^ Z[i][13];
+		DZ[i][10] = Z[0][10] ^ Z[i][10];
+		DZ[i][7] = Z[0][7] ^ Z[i][7];
+
+		// DZ_2^i[0,7,10,13] -> DX_3^i
+		DX[i][0] = MUL(DZ[i][0], 0x02);
+		DX[i][1] = DZ[i][0];
+		DX[i][2] = DZ[i][0];
+		DX[i][3] = MUL(DZ[i][0], 0x03);
+
+		DX[i][4] = DZ[i][7];
+		DX[i][5] = DZ[i][7];
+		DX[i][6] = MUL(DZ[i][7], 0x03);
+		DX[i][7] = MUL(DZ[i][7], 0x02);
+
+		DX[i][8] = DZ[i][10];
+		DX[i][9] = MUL(DZ[i][10], 0x03);
+		DX[i][10] = MUL(DZ[i][10], 0x02);
+		DX[i][11] = DZ[i][10];
+
+		DX[i][12] = MUL(DZ[i][13], 0x03);
+		DX[i][13] = MUL(DZ[i][13], 0x02);
+		DX[i][14] = DZ[i][13];
+		DX[i][15] = DZ[i][13];
+
+		// Step 1-(d). of Second Phase.
+		// X_3^0, DX_3^i -> X_3^i
+		for (int j = 0; j < 16; j++) X[i][j] = x[3][j] ^ DX[i][j];
+
+		// X_3^i -> Z_3^i
+		for (int j = 0; j < 16; j++) Z[i][j] = Sbox[X[i][(j + (j % 4) * 4) % 16]];
+
+		// Step 1-(e). of Second Phase.
+		// Z_3^0, Z_3^i -> DZ_3^i
+		for (int j = 0; j < 16; j++) DZ[i][j] = Z[0][j] ^ Z[i][j];
+
+		// DZ_3^i -> DX_4^i[0,5,10,15]
+		DX[i][0] = MUL(DZ[i][0], 0x02) ^ MUL(DZ[i][1], 0x03) ^ DZ[i][2] ^ DZ[i][3];
+		DX[i][5] = DZ[i][4] ^ MUL(DZ[i][5], 0x02) ^ MUL(DZ[i][6], 0x03) ^ DZ[i][7];
+		DX[i][10] = DZ[i][8] ^ DZ[i][9] ^ MUL(DZ[i][10], 0x02) ^ MUL(DZ[i][11], 0x03);
+		DX[i][15] = MUL(DZ[i][12], 0x03) ^ DZ[i][13] ^ DZ[i][14] ^ MUL(DZ[i][15], 0x02);
+
+		// Step 1-(f). of Second Phase.
+		// X_4^0[0,5,10,15], DX_4^i[0,5,10,15] -> X_4^i[0,5,10,15]
+		X[i][0] = x[4][0] ^ DX[i][0];
+		X[i][5] = x[4][5] ^ DX[i][5];
+		X[i][10] = x[4][10] ^ DX[i][10];
+		X[i][15] = x[4][15] ^ DX[i][15];
+
+		// X_4^i[0,5,10,15] -> Z_4^i[0,1,2,3]
+		Z[i][0] = Sbox[X[i][0]];
+		Z[i][1] = Sbox[X[i][5]];
+		Z[i][2] = Sbox[X[i][10]];
+		Z[i][3] = Sbox[X[i][15]];
+
+		// Step 1-(g). of Second Phase.
+		// Z_4^0[0,1,2,3], Z_4^i[0,1,2,3] -> DZ_4^i[0,1,2,3]
+		DZ[i][0] = Z[0][0] ^ Z[i][0];
+		DZ[i][1] = Z[0][1] ^ Z[i][1];
+		DZ[i][2] = Z[0][2] ^ Z[i][2];
+		DZ[i][3] = Z[0][3] ^ Z[i][3];
+
+		// DZ_4^i[0,1,2,3] -> C^i[0]
+		DW[i][0] = MUL(DZ[i][0], 0x02) ^ MUL(DZ[i][1], 0x03) ^ DZ[i][2] ^ DZ[i][3];
+
+		// Step 2. of Second Phase.
+		fprintf(in, "%d ", DW[i][0]);
 	}
+	fprintf(in, "\n");
+	seq_cnt++;
 }
